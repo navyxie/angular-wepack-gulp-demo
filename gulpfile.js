2cdf128eb5c45b4a2ca711da887e68b3d889dc50
navyxie <navyxie2010@gmail.com>
2016-08-04 19:53:28 +0800

    添加gulp处理构建流程

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,14 @@
 var gulp =require('gulp');
-var fs = require('fs');
-var path = require('path');
 var shell = require('gulp-shell');
-var webpack = require('gulp-webpack');
 var clean = require('gulp-clean');
+
 gulp.task('clean', function () {
     return gulp.src(['assets/dist'])
         .pipe(clean())
 });
 
-gulp.task('webpack', ['clean'], shell.task(['webpack']));
+gulp.task('webpack', shell.task(['webpack --watch']));
 
-gulp.task('watchjs', function (done) {
-    gulp.watch('assets/js/app/**/*.js', ['webpack']).on('end', done);
-});
+gulp.task('dev', ['webpack']);
 
-gulp.task('dev', ['watchjs']);
-
-gulp.task('default', ['webpack']);
+gulp.task('default', ['clean','webpack']);
