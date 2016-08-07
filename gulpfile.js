ff72d2d4038dc18be3adacb950f26053291391c2
navyxie <navyxie2010@gmail.com>
2016-08-08 00:25:57 +0800

    添加版本控制

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 var gulp =require('gulp');
 var shell = require('gulp-shell');
 var clean = require('gulp-clean');
+var RevAll = require('gulp-rev-all');
+var revAll = new RevAll();
 
 gulp.task('clean', function () {
     return gulp.src(['assets/dist'])
         .pipe(clean())
 });
+gulp.task('version', ['webpack'], function () {
+  return gulp.src('assets/dist/**/*.*')
+    .pipe(clean())
+    .pipe(revAll.revision())
+    .pipe(gulp.dest('assets/dist/'))
+    .pipe(revAll.manifestFile())
+    .pipe(gulp.dest(''));
+});
 
-gulp.task('webpack', shell.task(['webpack --watch']));
+gulp.task('webpack', shell.task(['webpack']));
+gulp.task('watch',  shell.task(['webpack --watch']));
 
-gulp.task('dev', ['webpack']);
+gulp.task('dev', ['watch']);
 
-gulp.task('default', ['clean','webpack']);
+gulp.task('default', ['clean','webpack','version']);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,37 @@ var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+
 var srcDir = path.resolve(process.cwd(), 'assets');
+
+//生成version文件
+function revManifest() {
+    this.plugin('done', function (stats) {
+        stats = stats.compilation.getStats().toJson({
+            hash: true,
+            publicPath: true,
+            assets: true,
+            chunks: false,
+            modules: false,
+            source: false,
+            errorDetails: false,
+            timings: false
+        });
+
+        var json = {}, chunk;
+        for (var key in stats.assetsByChunkName) {
+            if (stats.assetsByChunkName.hasOwnProperty(key)) {
+                chunk = stats.assetsByChunkName[key];
+                json[key + '.js'] = chunk[0];
+            }
+        }
+
+        fs.writeFileSync(
+            path.join(__dirname, 'rev-manifest.json'),
+            JSON.stringify(json, null, 2)
+        );
+    });
+}
 
 function getEntry () {
   var jsPath = path.resolve(srcDir,'js/app');
@@ -28,18 +58,21 @@ var entry = getEntry();
 entry['cookie__'] = ['cookie','_'];
 
 module.exports = {
-  devtool: "source-map",
+  // devtool: "source-map",//开发的时候设置为eval
+  devtool: "eval",//开发的时候设置为eval
   entry: entry,
   output: {
     path: path.join(__dirname, 'assets/dist/js/'),
     // publicPath: 'assets/dist/js/',
+    // filename: "[name].[chunkhash].js"
     filename: "[name].js"
   },
   module: {
     loaders: [
       { test: require.resolve(srcDir + '/js/common/angular/angular.js'), loader: "exports?window.angular" },
       { test: require.resolve(srcDir + '/js/app/util.js'), loader: "exports?window.util" },
-      { test: /\.css$/, loader: 'style-loader!css-loader'}
+      //{ test: /\.css$/, loader: 'style-loader!css-loader'},
+      { test: /\.css$/,loader: ExtractTextPlugin.extract("style-loader", "css-loader")},//不打包css
     ],
     noParse: [srcDir + '/js/common/angular/angular.js',srcDir + '/js/common/cookie/cookie.js',srcDir + '/js/common/underscore/1.6.0/underscore.js']
   },
@@ -59,11 +92,16 @@ module.exports = {
       _: '_'
     }),
     new CommonsChunkPlugin('common.js'),
+    // new CommonsChunkPlugin('common.[hash].js'),
     new uglifyJsPlugin({
       compress: {
         warnings: false
       }
-    })
+    }),
+    new ExtractTextPlugin("[name].css"),//不打包css
+    // function () {
+    //   revManifest.call(this);
+    // }
   ],
   // externals: {
   //   'angular': true
